3f30b6bda8542d34ae9275d72bf5e8d1ef01c426
Mauricio Chara <mazalsoft@gmail.com>
2023-06-19 16:24:57 -0500

    CUSTOMIZE THE ABOUT ME COMPONENT
    The about me component was customized, highlighting the main skills and the area of links of interest was also successfully customized.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,23 +5,22 @@ const AboutUs = () => {
     <SectionContainer
       name={"about"}
       extraClass="about-section"
-      title={"About"}
-      subTitle={"WHO I AM"}
-      leftImage="static/img/title-1.jpg"
-      leftImageTitle={"About Me"}
+      title={"Acerca de mí"}
+      subTitle={"QUIEN SOY"}
+      leftImage="static/img/title-1.png"
+      leftImageTitle={"@mauriciollc"}
     >
       <div className="row">
         <div className="col-md-4">
-          <img src="static/img/my-pic.jpg" title alt />
+          <img src="static/img/my-pic.png" title alt />
         </div>
         <div className="col-md-8 md-m-30px-t">
           <div className="about-text">
-            <h3 className="dark-color">I'm Alexis Larten</h3>
+            <h3 className="dark-color">Soy Mauricio Chara</h3>
             <p className="m-0px">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              Un desarrollador de aplicaciones versátil con amplia experiencia en construir y diseñar soluciones informáticas
+              para el uso en la web y en dispositivos móviles. Me destaco por mis habilidades de problem solving
+              y mi orientación al trabajo en equipo, impulsando proyectos innovadores y soluciones de alta calidad.
             </p>
           </div>{" "}
           {/* about-text */}
@@ -30,10 +29,9 @@ const AboutUs = () => {
               <div className="feature-box">
                 <i className="icon dark-color theme-after ti-ruler-pencil" />
                 <div className="feature-content">
-                  <h5 className="dark-color">Development</h5>
+                  <h5 className="dark-color">Desarrollo</h5>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt.
+                  Soy un desarrollador de software con experiencia en la creación de aplicaciones web, móviles y de escritorio orientadas a la facilidad de uso y al rendimiento.
                   </p>
                 </div>
               </div>
@@ -43,10 +41,9 @@ const AboutUs = () => {
               <div className="feature-box">
                 <i className="icon dark-color theme-after ti-image" />
                 <div className="feature-content">
-                  <h5 className="dark-color">Graphic</h5>
+                  <h5 className="dark-color">Audiovisual</h5>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt.
+                  Soy un especialista en Marketing Digital con amplia experiencia en desarrollo de estrategias de marketing digital, incluyendo campañas de publicidad en línea, contenido, SEO y redes sociales.
                   </p>
                 </div>
               </div>
@@ -56,10 +53,10 @@ const AboutUs = () => {
               <div className="feature-box">
                 <i className="icon dark-color theme-after ti-brush-alt" />
                 <div className="feature-content">
-                  <h5 className="dark-color">Web design</h5>
+                  <h5 className="dark-color">Componentes web</h5>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt.
+                  
+Soy un desarrollador de complementos con mucha experiencia, especializado en crear herramientas útiles y rápidas que se integran fácilmente en sitios web modernos.
                   </p>
                 </div>
               </div>
@@ -69,10 +66,9 @@ const AboutUs = () => {
               <div className="feature-box">
                 <i className="icon dark-color theme-after ti-mobile" />
                 <div className="feature-content">
-                  <h5 className="dark-color">Mobile apps</h5>
+                  <h5 className="dark-color">Aplicaciones móviles</h5>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt.
+                  Soy un desarrollador de aplicaciones full stack con experiencia en la creación de aplicaciones Android e iOS.
                   </p>
                 </div>
               </div>
@@ -82,7 +78,7 @@ const AboutUs = () => {
           {/* row */}
           <div className="btn-bar">
             <a href="#" className="btn btn-theme">
-              Download CV
+            Mi tienda virtual
             </a>
           </div>
         </div>
@@ -90,7 +86,7 @@ const AboutUs = () => {
       {/* row */}
       {/* 
      ==========================
-       Counter
+       Links of interest
      ==========================
      */}
       <div className="counter-row m-50px-t p-40px-t lg-m-35px-t lg-p-25px-t sm-p-15px-t">
@@ -98,9 +94,9 @@ const AboutUs = () => {
           <div className="col-md-3 col-sm-6 md-m-15px-tb">
             <div className="counter-col counter-box">
               <div className="counter-data" data-count={375}>
-                <i className="theme-color ti-face-smile" />
-                <div className="count dark-color">375</div>
-                <h6>Happy Clients</h6>
+                <i className="theme-color ti-comments-smiley" />
+                <div className="count dark-color"><a href="#" className="">¡Vamos a chatear!</a></div>
+                <h6>+57 3153774638</h6>
               </div>
             </div>
           </div>{" "}
@@ -108,9 +104,9 @@ const AboutUs = () => {
           <div className="col-md-3 col-sm-6 md-m-15px-tb">
             <div className="counter-col counter-box">
               <div className="counter-data" data-count={375}>
-                <i className="theme-color ti-headphone" />
-                <div className="count dark-color">375</div>
-                <h6>Telephonic Talk</h6>
+                <i className="theme-color ti-video-clapper" />
+                <div className="count dark-color"><a href="#" className="">¡Es hora de aprender!</a></div>
+                <h6>Membresía</h6>
               </div>
             </div>
           </div>{" "}
@@ -118,9 +114,9 @@ const AboutUs = () => {
           <div className="col-md-3 col-sm-6 md-m-15px-tb">
             <div className="counter-col counter-box">
               <div className="counter-data" data-count={375}>
-                <i className="theme-color ti-camera" />
-                <div className="count dark-color">375</div>
-                <h6>Photo Capture</h6>
+                <i className="theme-color ti-youtube" />
+                <div className="count dark-color"><a href="#" className="">¡Aprende en Youtube!</a></div>
+                <h6>Suscríbete a mi canal</h6>
               </div>
             </div>
           </div>{" "}
@@ -128,9 +124,9 @@ const AboutUs = () => {
           <div className="col-md-3 col-sm-6 md-m-15px-tb">
             <div className="counter-col counter-box">
               <div className="counter-data" data-count={375}>
-                <i className="theme-color ti-thumb-up" />
-                <div className="count dark-color">375</div>
-                <h6>Projects</h6>
+                <i className="theme-color ti-twitter" />
+                <div className="count dark-color"><a href="#" className="">¡Sigue mis Tweets!</a></div>
+                <h6>Sígueme en Twitter</h6>
               </div>
             </div>
           </div>{" "}
