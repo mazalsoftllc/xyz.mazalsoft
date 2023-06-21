b003af92f8d66bc1d0a793eef532a732a210499c
Mauricio Chara <mazalsoft@gmail.com>
2023-06-20 23:21:19 -0500

    PORTFOLIO OF CORPORATE PROJECTS
    The project portfolio component has been successfully created. The first project of the new digital age of Mazalsoft is the web application mazalsoft.xyz

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -47,9 +47,9 @@ const Portfolio = () => {
   return (
     <SectionContainer
       name={"portfolio"}
-      title={"My Projects"}
-      subTitle={"Latest Work"}
-      leftImage="static/img/title-3.jpg"
+      title={"Mis proyectos"}
+      subTitle={"Portafolio destacado"}
+      leftImage="static/img/title-2.png"
     >
       <div className="portfolio-section">
         <div className="portfolio-filter m-10px-b">
@@ -59,39 +59,49 @@ const Portfolio = () => {
               className={`${activeBtn("*")} theme-after`}
               onClick={handleFilterKeyChange("*")}
             >
-              All
+              Todo
             </li>{" "}
             <li
               className={`${activeBtn("photoshop")} theme-after`}
               onClick={handleFilterKeyChange("photoshop")}
               data-filter=".photoshop"
             >
-              Photoshop
+              Apps de escritorio
             </li>{" "}
             <li
               className={`${activeBtn("website")} theme-after`}
               onClick={handleFilterKeyChange("website")}
               data-filter=".website"
             >
-              Website
+              Apps móviles
             </li>{" "}
             <li
               className={`${activeBtn("apps")} theme-after`}
               onClick={handleFilterKeyChange("apps")}
               data-filter=".apps"
             >
-              Apps
+              Audiovisual
             </li>
+
+            <li
+              className={`${activeBtn("components")} theme-after`}
+              onClick={handleFilterKeyChange("components")}
+              data-filter=".components"
+            >
+              Componentes
+            </li>
+ 
+
           </ul>
         </div>{" "}
         {/* Portfolio Filter */}
         <div className="portfolio-content">
           <ul className="portfolio-cols portfolio-cols-3">
-            <li className="portfolio-item website">
+            <li className="portfolio-item components">
               <div className="portfolio-col portfolio-hover-01">
                 <div className="portfolio-img">
                   <a href="#">
-                    <img src="static/img/portfolio-1.jpg" title alt />
+                    <img src="static/img/portfolio-1.png" title alt />
                   </a>
                   <div className="hover">
                     <div className="action-btn">
@@ -103,12 +113,12 @@ const Portfolio = () => {
                       </a>
                       <a
                         className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-1.jpg"
+                        href="static/img/portfolio-1.png"
                         title="Lightbox gallery image title..."
                       >
                         <i className="fas fa-expand" />
                       </a>
-                      <a href="#" className="theme-color">
+                      <a href="https://www.udemy.com/user/edgar-mauricio-chara-hurtado/" className="theme-color">
                         <i className="fa fa-link" />
                       </a>
                     </div>{" "}
@@ -117,198 +127,14 @@ const Portfolio = () => {
                   {/* Hover */}
                 </div>
                 <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
+                  <h5>Mazalsoft</h5>
+                  <span>App web mazalsoft.xyz</span>
                 </div>
               </div>{" "}
               {/* Portfolio */}
             </li>{" "}
-            {/* col */}
-            <li className="portfolio-item apps">
-              <div className="portfolio-col portfolio-hover-01">
-                <div className="portfolio-img">
-                  <a href="#">
-                    <img src="static/img/portfolio-2.jpg" title alt />
-                  </a>
-                  <div className="hover">
-                    <div className="action-btn">
-                      <a
-                        href="http://www.youtube.com/watch?v=0O2aH4XLbto"
-                        className="popup-video theme-color"
-                      >
-                        <i className="fa fa-play" />
-                      </a>
-                      <a
-                        className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-2.jpg"
-                        title="Lightbox gallery image title..."
-                      >
-                        <i className="fas fa-expand" />
-                      </a>
-                      <a href="#" className="theme-color">
-                        <i className="fa fa-link" />
-                      </a>
-                    </div>{" "}
-                    {/* Video Btn */}
-                  </div>{" "}
-                  {/* Hover */}
-                </div>
-                <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
-                </div>
-              </div>{" "}
-              {/* Portfolio */}
-            </li>{" "}
-            {/* col */}
-            <li className="portfolio-item photoshop apps">
-              <div className="portfolio-col portfolio-hover-01">
-                <div className="portfolio-img">
-                  <a href="#">
-                    <img src="static/img/portfolio-3.jpg" title alt />
-                  </a>
-                  <div className="hover">
-                    <div className="action-btn">
-                      <a
-                        href="http://www.youtube.com/watch?v=0O2aH4XLbto"
-                        className="popup-video theme-color"
-                      >
-                        <i className="fa fa-play" />
-                      </a>
-                      <a
-                        className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-3.jpg"
-                        title="Lightbox gallery image title..."
-                      >
-                        <i className="fas fa-expand" />
-                      </a>
-                      <a href="#" className="theme-color">
-                        <i className="fa fa-link" />
-                      </a>
-                    </div>{" "}
-                    {/* Video Btn */}
-                  </div>{" "}
-                  {/* Hover */}
-                </div>
-                <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
-                </div>
-              </div>{" "}
-              {/* Portfolio */}
-            </li>{" "}
-            {/* col */}
-            <li className="portfolio-item photoshop website">
-              <div className="portfolio-col portfolio-hover-01">
-                <div className="portfolio-img">
-                  <a href="#">
-                    <img src="static/img/portfolio-4.jpg" title alt />
-                  </a>
-                  <div className="hover">
-                    <div className="action-btn">
-                      <a
-                        href="http://www.youtube.com/watch?v=0O2aH4XLbto"
-                        className="popup-video theme-color"
-                      >
-                        <i className="fa fa-play" />
-                      </a>
-                      <a
-                        className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-4.jpg"
-                        title="Lightbox gallery image title..."
-                      >
-                        <i className="fas fa-expand" />
-                      </a>
-                      <a href="#" className="theme-color">
-                        <i className="fa fa-link" />
-                      </a>
-                    </div>{" "}
-                    {/* Video Btn */}
-                  </div>{" "}
-                  {/* Hover */}
-                </div>
-                <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
-                </div>
-              </div>{" "}
-              {/* Portfolio */}
-            </li>{" "}
-            {/* col */}
-            <li className="portfolio-item photoshop apps">
-              <div className="portfolio-col portfolio-hover-01">
-                <div className="portfolio-img">
-                  <a href="#">
-                    <img src="static/img/portfolio-5.jpg" title alt />
-                  </a>
-                  <div className="hover">
-                    <div className="action-btn">
-                      <a
-                        href="http://www.youtube.com/watch?v=0O2aH4XLbto"
-                        className="popup-video theme-color"
-                      >
-                        <i className="fa fa-play" />
-                      </a>
-                      <a
-                        className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-5.jpg"
-                        title="Lightbox gallery image title..."
-                      >
-                        <i className="fas fa-expand" />
-                      </a>
-                      <a href="#" className="theme-color">
-                        <i className="fa fa-link" />
-                      </a>
-                    </div>{" "}
-                    {/* Video Btn */}
-                  </div>{" "}
-                  {/* Hover */}
-                </div>
-                <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
-                </div>
-              </div>{" "}
-              {/* Portfolio */}
-            </li>{" "}
-            {/* col */}
-            <li className="portfolio-item app website">
-              <div className="portfolio-col portfolio-hover-01">
-                <div className="portfolio-img">
-                  <a href="#">
-                    <img src="static/img/portfolio-6.jpg" title alt />
-                  </a>
-                  <div className="hover">
-                    <div className="action-btn">
-                      <a
-                        href="http://www.youtube.com/watch?v=0O2aH4XLbto"
-                        className="popup-video theme-color"
-                      >
-                        <i className="fa fa-play" />
-                      </a>
-                      <a
-                        className="lightbox-gallery theme-color"
-                        href="static/img/portfolio-6.jpg"
-                        title="Lightbox gallery image title..."
-                      >
-                        <i className="fas fa-expand" />
-                      </a>
-                      <a href="#" className="theme-color">
-                        <i className="fa fa-link" />
-                      </a>
-                    </div>{" "}
-                    {/* Video Btn */}
-                  </div>{" "}
-                  {/* Hover */}
-                </div>
-                <div className="portfolio-info">
-                  <h5>Portpolio Template</h5>
-                  <span>Resent Work</span>
-                </div>
-              </div>{" "}
-              {/* Portfolio */}
-            </li>{" "}
-            {/* col */}
+            {/* col */}  
+          
           </ul>{" "}
           {/* row */}
         </div>{" "}
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,23 +4,39 @@ const Testimonials = () => {
   return (
     <div className="testimonial-section m-30px-t sm-m-20px-t pb-5">
       <div className="sub-title m-30px-b">
-        <h2 className="dark-color theme-after">What People Say?</h2>
+        <h2 className="dark-color theme-after">Comunicados oficiales</h2>
       </div>
       <Slider {...sliderProps.testimonial} id="client-slider-single">
         <div className="testimonial-col">
           <div className="say">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s.
+              Mazalsoft está comprometido con la oferta de soluciones integrales en comercio electrónico a empresas y emprendedores en América del Sur. Por lo tanto, anunciamos que a partir de ahora, cualquier cliente que desee crear una tienda de comercio electrónico en América del Sur deberá disponer de una cuenta habilitada con Stripe. 
+
             </p>
           </div>
           <div className="user">
             <div className="img">
-              <img src="static/img/avtar1.jpg" alt title />
+              <img src="static/img/avtar1.png" alt title />
             </div>
             <div className="name ml-2">
-              <span>Jennifer Lutheran</span>
+              <span>Mauricio Chara</span>
+              <label>CEO</label>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial-col">
+          <div className="say">
+            <p>Queridos estudiantes de Mazalsoft,
+               Estamos muy entusiasmados y emocionados de anunciarles que hemos alcanzado los 5,000 alumnos en nuestra Learning Management System de Udemy.
+               Nuestros planes de capacitación en línea se han expandido de manera significativa desde su comienzo hace un año. Gracias a nuestra comunidad de estudiantes y sus apoyo.
+            </p>
+          </div>
+          <div className="user">
+            <div className="img">
+              <img src="static/img/avtar1.png" alt title />
+            </div>
+            <div className="name ml-2">
+              <span>Mauricio Chara</span>
               <label>CEO</label>
             </div>
           </div>
@@ -28,35 +44,15 @@ const Testimonials = () => {
         <div className="testimonial-col">
           <div className="say">
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s.
+            Estamos encantados de anunciar que Mazalsoft ha actualizado su lenguaje de programación principal a Flutter. Esta eficiente herramienta de desarrollo nos permitirá optimizar en tiempo de desarrollo, generar aplicaciones con mayor alcance de plataformas. Más aún, será posible trabajar con una sola línea de código, permitiendo una completa atención y soporte al cliente.
             </p>
           </div>
           <div className="user">
             <div className="img">
-              <img src="static/img/avtar1.jpg" alt title />
+              <img src="static/img/avtar1.png" alt title />
             </div>
             <div className="name ml-2">
-              <span>Salma Hayek</span>
-              <label>CEO</label>
-            </div>
-          </div>
-        </div>
-        <div className="testimonial-col">
-          <div className="say">
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s.
-            </p>
-          </div>
-          <div className="user">
-            <div className="img">
-              <img src="static/img/avtar1.jpg" alt title />
-            </div>
-            <div className="name ml-2">
-              <span>Martin Lutheran</span>
+              <span>Mauricio Chara</span>
               <label>CEO</label>
             </div>
           </div>
